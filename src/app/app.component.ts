953418300044405ac04f44a9b484e83b00850374
Draike13 <tmbiri91@gmail.com>
2022-11-09 10:38:04 -0600

    single card output working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'Card-Tracker';
+  cards: any;
+
+  constructor(public http: HttpClient) {
+    this.http
+      .get(
+        'https://api.pokemontcg.io/v2/cards?q=name:eevee&orderBy=id&key=20d4e68d-e4a9-487f-a80a-9120e2012c39'
+      )
+      .subscribe((cards) => (this.cards = cards));
+  }
+  findCard() {
+    console.log(this.cards.data[0].images.small);
+  }
 }
